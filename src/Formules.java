586ccf0a57a8fcfc7579da6edb1502901f357bb3
AzzazHichem22 <eh_azzaz@esi.dz>
2017-10-15 09:39:26 +0100

    La classe Formule a été crée

---
+++ src/Formules.java
@@ -0,0 +1,17 @@
+
+public class Formules {
+
+	public int Doubler(int a)
+	{
+		return a*2;
+	}
+	public int Incrementer(int a)
+	{
+		return a+1;
+	}
+	public int Decrementer(int a)
+	{
+		return a-1;
+	}
+	
+}
