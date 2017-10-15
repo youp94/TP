4a914cca2c1482cdf74ea25e3acc3e612d353fae
youp94 <dm_benchadi@esi.dz>
2017-10-15 02:13:38 +0100

    Test de la méthode tri:
    La méthode marche

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Vector;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Vector<Integer> v = new Vector<>();
+		v.add(5);
+		v.add(4);
+		v.add(3);
+		v.add(10);
+		v.add(6);
+		VectorHelper.tri(v);
+		for(int i=0;i<v.size();i++) {
+			System.out.println(v.elementAt(i));
+		}
 	}
 
 }
--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -11,8 +11,8 @@ public class VectorHelper {
 			for (int i = 0; i < longueur - 1; i++) {
 				if (v.elementAt(i) > v.elementAt(i+1)) {
 					tampon = v.elementAt(i);
-					v.insertElementAt(v.elementAt(i+1), i);
-					v.insertElementAt(tampon, i+1);
+					v.setElementAt(v.elementAt(i+1), i);
+					v.setElementAt(tampon, i+1);
 					permut = true;
 				}
 			}
