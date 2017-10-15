0286e329ccbe8511e8549d418bdfcf0ee1c388a4
youp94 <dm_benchadi@esi.dz>
2017-10-15 02:32:44 +0100

    Une modification.

--- src/SizeException.java
+++ src/SizeException.java
@@ -1,5 +1,10 @@
 
 public class SizeException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public SizeException(String message) {
         super(message);
     }
