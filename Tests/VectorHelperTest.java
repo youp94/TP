3935b288368a7167540e887afdd2f0b6f15312f6
youp94 <dm_benchadi@esi.dz>
2017-10-19 01:27:26 +0100

    Test de tous les méthodes

--- Tests/VectorHelperTest.java
+++ Tests/VectorHelperTest.java
@@ -9,9 +9,43 @@ import org.junit.Test;
 public class VectorHelperTest {
 
 	Vector<Integer> v = new Vector<>();
+	Vector<Integer> v2 = new Vector<>();
+	Vector<Integer> v3 = new Vector<>();
+	Vector<Integer> v4 = new Vector<>();
+	Vector<Integer> res = new Vector<>();
+	Integer[] tab = {3,10};
 	
 	@Before
 	public void setUp() throws Exception {
+		v.add(5);
+		v.add(4);
+		v.add(3);
+		v.add(10);
+		v.add(6);
+		
+		v2.add(3);
+		v2.add(4);
+		v2.add(5);
+		v2.add(6);
+		v2.add(10);
+		
+		v3.add(6);
+		v3.add(10);
+		v3.add(3);
+		v3.add(4);
+		v3.add(5);
+		
+		v4.add(10);
+		v4.add(8);
+		v4.add(6);
+		v4.add(20);
+		v4.add(12);
+		
+		res.add(10);
+		res.add(8);
+		res.add(6);
+		res.add(20);
+		res.add(12);
 	}
 
 	@After
@@ -20,7 +54,31 @@ public class VectorHelperTest {
 
 	@Test
 	public void testTri() {
-		assertEquals(1,1);
+		VectorHelper.tri(v);
+		 assertEquals(v2,v);
+	}
+	
+	@Test
+	public void testSomme() throws SizeException {
+		 assertEquals(res,VectorHelper.somme(v, v));
+	}
+	
+	@Test
+	public void testInvese() {
+		VectorHelper.invese(v);
+		 assertEquals(v3,v);
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testMinMax() {
+		 assertEquals(tab,VectorHelper.MinMax(v));
+	}
+	
+	@Test
+	public void testAppliquerFormule() {
+		VectorHelper.AppliquerFormule(v);
+		 assertEquals(v4,v);
 	}
 
 }
